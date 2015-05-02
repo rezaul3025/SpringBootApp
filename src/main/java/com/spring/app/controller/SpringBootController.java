59f95a04cc82e5e0710de8dcc8579e71b682acbf
rezaul3025 <rezaul.km105@gmail.com>
2015-05-02 23:49:58 +0200

    add domain obects and jpa transction

--- src/main/java/com/spring/app/controller/SpringBootController.java
+++ src/main/java/com/spring/app/controller/SpringBootController.java
@@ -1,14 +1,28 @@
 package com.spring.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.app.core.domain.Student;
+import com.spring.app.service.StudentService;
 
 @Controller
 public class SpringBootController {
 	
+	@Autowired
+	private StudentService stdService;
+	
 	@RequestMapping(value="/")
 	public String index()
 	{
+		Student st= new Student();
+		
+		st.setName("Tom mia");
+		st.setDept("IT US");
+		
+		stdService.addStudent(st);
+		
 		return "index";
 	}
 
